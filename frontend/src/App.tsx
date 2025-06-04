9b2130f876f57fc01ec4215bf001823512e60c9c
ash <ashleyjpedu@gmail.com>
2025-06-05 01:23:04 +1000

    project restructure to accommodate backend addition

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import CreateReport from './pages/CreateReport';
+import Profile from './pages/Profile';
+import AllIncidents from './pages/AllIncidents';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/all-incidents" element={<AllIncidents />} />
+        <Route path="/create-report" element={<CreateReport />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
