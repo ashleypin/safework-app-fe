88bebcb2f47249682099b67dad5de87cf9b2ca61
ash <ashleyjpedu@gmail.com>
2025-06-05 11:14:43 +1000

    fix incident routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import CreateReport from './pages/CreateReport';
-import ManageWorkplace from './pages/ManageWorkplace';
 import Profile from './pages/Profile';
-import AllIncidents from './pages/AllIncidents';
+import Incidents from './pages/Incidents';
 
 function App() {
   return (
@@ -11,9 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/dashboard" />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/all-incidents" element={<AllIncidents />} />
+        <Route path="/incidents" element={<Incidents />} />
         <Route path="/create-report" element={<CreateReport />} />
-        <Route path="/manage-workplace" element={<ManageWorkplace />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
