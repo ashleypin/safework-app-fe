32de3d041df8dff6ce0669280ff253cf248393e2
ash <ashleyjpedu@gmail.com>
2025-06-05 10:21:55 +1000

    remove workplace page - temp

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import CreateReport from './pages/CreateReport';
-import ManageWorkplace from './pages/ManageWorkplace';
 import Profile from './pages/Profile';
 import AllIncidents from './pages/AllIncidents';
 
@@ -13,7 +12,6 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/all-incidents" element={<AllIncidents />} />
         <Route path="/create-report" element={<CreateReport />} />
-        <Route path="/manage-workplace" element={<ManageWorkplace />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
