3055e394ba7743cd7e64cdbe9b7efa9fd64d87a8
ash <ashleyjpedu@gmail.com>
2025-06-18 13:07:58 +1000

    add IncidentDetail route

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Dashboard from './pages/Dashboard';
 import CreateReport from './pages/CreateReport';
 import Profile from './pages/Profile';
 import Incidents from './pages/Incidents';
+import IncidentDetail from './pages/IncidentDetail';
 import Workplaces from './pages/Workplaces';
-
 function App() {
   return (
     <BrowserRouter>
@@ -12,6 +12,7 @@ function App() {
         <Route path="/" element={<Navigate to="/dashboard" />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/incidents" element={<Incidents />} />
+        <Route path="/incidents/:id" element={<IncidentDetail />} />
         <Route path="/create-report" element={<CreateReport />} />
         <Route path="/workplaces" element={<Workplaces />} />
         <Route path="/profile" element={<Profile />} />
